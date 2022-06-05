5d0f2f6cc7bb8b59ea1f7e21c2b27090f44957e2
Smightym8 <michaelspiegel@live.at>
2022-06-05 16:40:44 +0200

    Implemented redis subscriber

--- src/main/java/at/fhv/ec/communication/queue/EventListener.java
+++ src/main/java/at/fhv/ec/communication/queue/EventListener.java
@@ -3,18 +3,25 @@ package at.fhv.ec.communication.queue;
 import at.fhv.ec.application.api.PurchaseService;
 import at.fhv.ss22.ea.f.communication.dto.DigitalProductPurchasedDTO;
 import com.google.gson.Gson;
+import io.quarkus.runtime.StartupEvent;
 import io.quarkus.scheduler.Scheduled;
 import org.eclipse.microprofile.config.inject.ConfigProperties;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPubSub;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import java.util.List;
 
 @ApplicationScoped
 public class EventListener {
+    @Inject
+    Logger logger;
+
     @Inject
     PurchaseService purchaseService;
 
@@ -27,8 +34,25 @@ public class EventListener {
     @ConfigProperty(name = "redis.port")
     int redisPort;
 
-    @Scheduled(every = "5s")
-    void receiveEvents() {
+    void receiveEvents(@Observes StartupEvent startupEvent) {
+        try(Jedis redisSubscriber = new Jedis(redisHost, redisPort)) {
+            JedisPubSub jedisPubSub = new JedisPubSub() {
+                @Override
+                public void onMessage(String channel, String message) {
+                    logger.info("Received event from channel: " + channel);
+
+                    if(channel.equalsIgnoreCase(purchaseEventQueueName)) {
+                        DigitalProductPurchasedDTO event = GSON.fromJson(message, DigitalProductPurchasedDTO.class);
+
+                        purchaseService.receivePurchase(event);
+                    }
+                }
+            };
+
+            redisSubscriber.subscribe(jedisPubSub, purchaseEventQueueName);
+        }
+
+        /*
         JedisPool jedisPool = new JedisPool(redisHost, redisPort);
 
         try(Jedis jedis = jedisPool.getResource()) {
@@ -44,5 +68,7 @@ public class EventListener {
                 }
             }
         }
+         */
+
     }
 }
